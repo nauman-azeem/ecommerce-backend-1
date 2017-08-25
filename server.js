560c9dfb26e89bbae151d94004a977f980015651
Nauman Azeem <borissims4@gmail.com>
2017-08-25 23:51:45 +0500

    Static files serving Functionality added

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet')
 const mongodb = require('mongodb');
-
+const path = require('path');
 
 var app = express();
-app.use(helmet());
-app.use(bodyParser.json());
-app.use('/staticfiles', express.static('staticfiles'));
+// app.use(helmet());
+// app.use(bodyParser.json());
+
+app.use('/static_files', express.static(path.join(__dirname, 'static_files')));
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
